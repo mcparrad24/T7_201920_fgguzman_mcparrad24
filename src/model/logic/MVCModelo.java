43a1cfab288534a5085e3da17924a139f767b343
FelipeGuzman42 <fg.guzman@uniandes.edu.co>
2019-08-23 13:50:12 -0500

    lector

--- src/model/logic/MVCModelo.java
+++ src/model/logic/MVCModelo.java
@@ -2,7 +2,11 @@ package model.logic;
 
 import model.data_structures.ArregloDinamico;
 import model.data_structures.IArregloDinamico;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
+ import com.opencsv.CSVReader
 /**
  * Definicion del modelo del mundo
  *
@@ -12,61 +16,86 @@ public class MVCModelo {
 	 * Atributos del modelo del mundo
 	 */
 	private IArregloDinamico datos;
-	
+
 	/**
 	 * Constructor del modelo del mundo con capacidad predefinida
 	 */
-	public MVCModelo()
-	{
+	public void CVSLector() {
+		CSVReader reader = null;
+		try {
+
+			reader = new CSVReader(new FileReader("./data/bogota-cadastral-2018-2-All-MonthlyAggregate.csv"));
+			for (String[] nextLine : reader) {
+				System.out.println("col1: " + nextLine[0] + ", col2: " + nextLine[1]);
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+	}
+
+	}
+
+	public MVCModelo() {
 		datos = new ArregloDinamico(7);
 	}
-	
+
 	/**
 	 * Constructor del modelo del mundo con capacidad dada
+	 * 
 	 * @param tamano
 	 */
-	public MVCModelo(int capacidad)
-	{
+	public MVCModelo(int capacidad) {
 		datos = new ArregloDinamico(capacidad);
 	}
-	
+
 	/**
-	 * Servicio de consulta de numero de elementos presentes en el modelo 
+	 * Servicio de consulta de numero de elementos presentes en el modelo
+	 * 
 	 * @return numero de elementos presentes en el modelo
 	 */
-	public int darTamano()
-	{
+	public int darTamano() {
 		return datos.darTamano();
 	}
 
 	/**
 	 * Requerimiento de agregar dato
+	 * 
 	 * @param dato
 	 */
-	public void agregar(String dato)
-	{	
+	public void agregar(String dato) {
 		datos.agregar(dato);
 	}
-	
+
 	/**
 	 * Requerimiento buscar dato
-	 * @param dato Dato a buscar
+	 * 
+	 * @param dato
+	 *            Dato a buscar
 	 * @return dato encontrado
 	 */
-	public String buscar(String dato)
-	{
+	public String buscar(String dato) {
 		return datos.buscar(dato);
 	}
-	
+
 	/**
 	 * Requerimiento eliminar dato
-	 * @param dato Dato a eliminar
+	 * 
+	 * @param dato
+	 *            Dato a eliminar
 	 * @return dato eliminado
 	 */
-	public String eliminar(String dato)
-	{
+	public String eliminar(String dato) {
 		return datos.eliminar(dato);
 	}
-
 
 }
