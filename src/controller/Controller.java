b63f93094c38c4149dce88b4fa7554052ab61a44
FelipeGuzman <fg.guzman@uniandes.edu.co>
2019-09-15 22:15:41 -0500

    punto 3C

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -29,6 +29,7 @@ public class Controller {
 		boolean fin = false;
 		int num = 0, j = 0;
 		String dato = "";
+		String trimestre = "5";
 		String[] datos;
 		String hora = "", N = "";
 
@@ -38,19 +39,16 @@ public class Controller {
 			int option = lector.nextInt();
 			switch (option) {
 			case 1:
-				System.out.println("--------- \nDar el nï¿½mero del trimestre deseado");
-				dato = lector.next();
-				modelo.CVSLector(Integer.parseInt(dato));
-				System.out.println(
-						"Total viajes por hora: " + modelo.totalHora() + "\n---------");
-				System.out.println(
-						"Total viajes por semana: " + modelo.totalSemana() + "\n---------");
-				System.out.println(
-						"Total viajes por mes: " + modelo.totalMes() + "\n---------");
-				System.out.println(
-						"Zona con menor identificador: " + Arrays.toString(modelo.menorIdentificador()) + "\n---------");
-				System.out.println(
-						"Zona con mayor identificador: " + Arrays.toString(modelo.mayorIdentificador()) + "\n---------");
+				System.out.println("--------- \nDar el numero del trimestre deseado");
+				trimestre = lector.next();
+				modelo.CVSLector(Integer.parseInt(trimestre));
+				System.out.println("Total viajes por hora: " + modelo.totalHora() + "\n---------");
+				System.out.println("Total viajes por semana: " + modelo.totalSemana() + "\n---------");
+				System.out.println("Total viajes por mes: " + modelo.totalMes() + "\n---------");
+				System.out.println("Zona con menor identificador: " + Arrays.toString(modelo.menorIdentificador())
+						+ "\n---------");
+				System.out.println("Zona con mayor identificador: " + Arrays.toString(modelo.mayorIdentificador())
+						+ "\n---------");
 				break;
 			case 2:
 				System.out.println("--------- \nHacer la consulta con:");
@@ -58,41 +56,57 @@ public class Controller {
 				System.out.println("2. Dia de la semana");
 				int eleccion = lector.nextInt();
 				if (eleccion == 1) {
-					System.out.println("--------- \nDar el ID en la zona de origen, el ID de la zona destino y el numero del mes (eg. 955,206,2");
+					System.out.println(
+							"--------- \nDar el ID en la zona de origen, el ID de la zona destino y el numero del mes (eg. 955,206,2");
 					String rta = lector.next();
-					String [] datosMes = rta.split(",");
+					String[] datosMes = rta.split(",");
 					String zonaO = datosMes[0];
 					String zonaD = datosMes[1];
 					String mes = datosMes[2];
 					String ans = modelo.tiempoPromedioDesviacionMes(zonaO, zonaD, mes);
 					if (ans == null) {
-						System.out.println("No existen viajes desde la zona de origen con ID " + zonaO + " hasta la zona destino con ID " + zonaD + " en el mes dado identificado con el numero " + mes);
-					}
-					else {
+						System.out.println("No existen viajes desde la zona de origen con ID " + zonaO
+								+ " hasta la zona destino con ID " + zonaD
+								+ " en el mes dado identificado con el numero " + mes);
+					} else {
 						String[] promedio = ans.split(",");
 						String tP = promedio[0];
 						String dE = promedio[1];
-						System.out.println("El tiempo promedio de los viajes desde la zona de origen con ID " + zonaO + " hasta la zona destino con ID " + zonaD + " en el mes dado identificado con el numero " + mes + " es: " + tP + "\n---------");
-						System.out.println("La desviacion estandar promedio de los viajes desde la zona de origen con ID " + zonaO + " hasta la zona destino con ID " + zonaD + " en el mes dado identificado con el numero " + mes + " es: " + dE + "\n---------");
+						System.out.println("El tiempo promedio de los viajes desde la zona de origen con ID " + zonaO
+								+ " hasta la zona destino con ID " + zonaD
+								+ " en el mes dado identificado con el numero " + mes + " es: " + tP + "\n---------");
+						System.out
+								.println("La desviacion estandar promedio de los viajes desde la zona de origen con ID "
+										+ zonaO + " hasta la zona destino con ID " + zonaD
+										+ " en el mes dado identificado con el numero " + mes + " es: " + dE
+										+ "\n---------");
 					}
-				}
-				else if (eleccion == 2) {
-					System.out.println("--------- \nDar el ID en la zona de origen, el ID de la zona destino y el numero del dia de la semana, siendo 1 el domingo (eg. 955,206,2");
+				} else if (eleccion == 2) {
+					System.out.println(
+							"--------- \nDar el ID en la zona de origen, el ID de la zona destino y el numero del dia de la semana, siendo 1 el domingo (eg. 955,206,2)");
 					String rta = lector.next();
-					String [] datosDia = rta.split(",");
+					String[] datosDia = rta.split(",");
 					String zonaO = datosDia[0];
 					String zonaD = datosDia[1];
 					String dia = datosDia[2];
 					String ans = modelo.tiempoPromedioDesviacionDia(zonaO, zonaD, dia);
 					if (ans == null) {
-						System.out.println("No existen viajes desde la zona de origen con ID " + zonaO + " hasta la zona destino con ID " + zonaD + " en el dia de la semana dado identificado con el numero " + dia);
-					}
-					else {
+						System.out.println("No existen viajes desde la zona de origen con ID " + zonaO
+								+ " hasta la zona destino con ID " + zonaD
+								+ " en el dia de la semana dado identificado con el numero " + dia);
+					} else {
 						String[] promedio = ans.split(",");
 						String tP = promedio[0];
 						String dE = promedio[1];
-						System.out.println("El tiempo promedio de los viajes desde la zona de origen con ID " + zonaO + " hasta la zona destino con ID " + zonaD + " en el dia de la semana dado identificado con el numero " + dia + " es: " + tP + "\n---------");
-						System.out.println("La desviacion estandar promedio de los viajes desde la zona de origen con ID " + zonaO + " hasta la zona destino con ID " + zonaD + " en el dia de la semana dado identificado con el numero " + dia + " es: " + dE + "\n---------");
+						System.out.println("El tiempo promedio de los viajes desde la zona de origen con ID " + zonaO
+								+ " hasta la zona destino con ID " + zonaD
+								+ " en el dia de la semana dado identificado con el numero " + dia + " es: " + tP
+								+ "\n---------");
+						System.out
+								.println("La desviacion estandar promedio de los viajes desde la zona de origen con ID "
+										+ zonaO + " hasta la zona destino con ID " + zonaD
+										+ " en el dia de la semana dado identificado con el numero " + dia + " es: "
+										+ dE + "\n---------");
 					}
 				}
 				break;
@@ -103,35 +117,36 @@ public class Controller {
 				System.out.println("3. Hora del dia");
 				int election = lector.nextInt();
 				if (election == 1) {
-					System.out.println("--------- \nDar el numero de viajes que desea consultar y el numero del mes (eg. 10,2");
+					System.out.println(
+							"--------- \nDar el numero de viajes que desea consultar y el numero del mes (eg. 10,2");
 					String nYM = lector.next();
 					String[] ans = nYM.split(",");
 					clus = modelo.tiempoPromViajesMes(ans[0], ans[1]);
 					System.out.println("Primeros " + ans[0] + " viajes: \n");
 					for (int i = 0; i < Integer.parseInt(ans[0]); i++) {
-						String [] actual = clus.dequeue();
+						String[] actual = clus.dequeue();
 						System.out.println(actual[0] + actual[1] + actual[2] + actual[3] + "\n---------");
 					}
-				}
-				else if (election == 2) {
-					System.out.println("--------- \nDar el numero de viajes que desea consultar y el numero del dia de la semana, siendo 1 el domingo (eg. 10,2");
+				} else if (election == 2) {
+					System.out.println(
+							"--------- \nDar el numero de viajes que desea consultar y el numero del dia de la semana, siendo 1 el domingo (eg. 10,2");
 					String nYD = lector.next();
 					String[] ans = nYD.split(",");
 					clus = modelo.tiempoPromViajesDia(ans[0], ans[1]);
 					System.out.println("Primeros " + ans[0] + " viajes: \n");
 					for (int i = 0; i < Integer.parseInt(ans[0]); i++) {
-						String [] actual = clus.dequeue();
+						String[] actual = clus.dequeue();
 						System.out.println(actual[0] + actual[1] + actual[2] + actual[3] + "\n---------");
 					}
-				}
-				else if (election == 3) {
-					System.out.println("--------- \nDar el numero de viajes que desea consultar y el numero de la hora del dia (eg. 10,2");
+				} else if (election == 3) {
+					System.out.println(
+							"--------- \nDar el numero de viajes que desea consultar y el numero de la hora del dia (eg. 10,2");
 					String nYH = lector.next();
 					String[] ans = nYH.split(",");
 					clus = modelo.tiempoPromViajesHora(ans[0], ans[1]);
 					System.out.println("Primeros " + ans[0] + " viajes: \n");
 					for (int i = 0; i < Integer.parseInt(ans[0]); i++) {
-						String [] actual = clus.dequeue();
+						String[] actual = clus.dequeue();
 						System.out.println(actual[0] + actual[1] + actual[2] + actual[3] + "\n---------");
 					}
 				}
@@ -143,16 +158,27 @@ public class Controller {
 				int seleccion = lector.nextInt();
 				if (seleccion == 1) {
 					System.out.println("--------- \nDar el ID de la zona y el numero del mes (eg. 110,2");
-				}
-				else if (seleccion == 2) {
-					System.out.println("--------- \nDar el ID de la zona y el numero del dia de la semana, siendo 1 el domingo (eg. 110,2");
+				} else if (seleccion == 2) {
+					System.out.println(
+							"--------- \nDar el ID de la zona y el numero del dia de la semana, siendo 1 el domingo (eg. 110,2");
 				}
 				break;
 			case 5:
-				System.out.println("--------- \nDar el ID de la zona de origen, el ID de la zona destino y la franja horaria entre parentesis (eg.955,206,(2-5)");
+				System.out.println(
+						"--------- \nDar el ID de la zona de origen, el ID de la zona destino y la franja horaria entre parentesis (eg.955,206,(2-5)");
 				break;
 			case 6:
-				System.out.println("--------- \nDar el ID de la zona de origen y el ID de la zona destino");
+				System.out.println("--------- \nDar el ID de la zona de origen y el ID de la zona destino (eg. 6,22)");
+				dato = lector.next();
+				datos = dato.split(",");
+				String[] resp = modelo.graficaASCII(datos[0], datos[1]);
+				System.out.println("Aproximación en minutos de viajes entre zona origen y zona destino.\n"
+						+ "Trimestre "+trimestre+" del 2018 detallado por cada hora del día.\n"
+						+ "Zona Origen: "+datos[0]+" Zona Destino: "+datos[1]+"\n"
+						+ "Hora|  # de minutos\n");
+				for (int i = 0; i < resp.length; i++) {
+					System.out.println(i+"  |  "+resp[i]+"\n");
+				}
 			case 7:
 				System.out.println("--------- \n Hasta pronto !! \n---------");
 				lector.close();
--- src/model/logic/MVCModelo.java
+++ src/model/logic/MVCModelo.java
@@ -18,15 +18,17 @@ public class MVCModelo {
 	private Queue<String[]> datosM = new Queue<>();
 	private Queue<String[]> datosW = new Queue<>();
 
+	// CARGA DE INFORMACIÓN
 	/**
 	 * Lector de los archivos de excel
+	 * 
 	 * @param numero de trimestre deseado
 	 */
 	public void CVSLector(int num) {
 		CSVReader reader = null;
-		String archivoH = "./data/bogota-cadastral-2018-"+num+"-All-HourlyAggregate.csv";
-		String archivoM = "./data/bogota-cadastral-2018-"+num+"-All-MonthlyAggregate.csv";
-		String archivoW = "./data/bogota-cadastral-2018-"+num+"-WeeklyAggregate.csv";
+		String archivoH = "./data/bogota-cadastral-2018-" + num + "-All-HourlyAggregate.csv";
+		String archivoM = "./data/bogota-cadastral-2018-" + num + "-All-MonthlyAggregate.csv";
+		String archivoW = "./data/bogota-cadastral-2018-" + num + "-WeeklyAggregate.csv";
 		String[] header = new String[1];
 		try {
 			reader = new CSVReader(new FileReader(archivoH));
@@ -57,24 +59,31 @@ public class MVCModelo {
 
 		}
 	}
+
 	/**
-	 * Los siguientes metodos buscan el tamano (total) de viajes segun el periodo de tiempo
+	 * Los siguientes metodos buscan el tamano (total) de viajes segun el periodo de
+	 * tiempo
+	 * 
 	 * @return El total (tamano)
 	 */
 	public int totalHora() {
 		int total = datosH.darTamano();
 		return total;
 	}
+
 	public int totalSemana() {
 		int total = datosW.darTamano();
 		return total;
 	}
+
 	public int totalMes() {
 		int total = datosM.darTamano();
 		return total;
 	}
+
 	/**
 	 * Los siguientes metodos buscan la zona deseada
+	 * 
 	 * @return la zona menor o mayor
 	 */
 	public String[] menorIdentificador() {
@@ -83,95 +92,101 @@ public class MVCModelo {
 		Queue<String[]> copia = datosH;
 		int menor = 99;
 		int test = 99;
-		for(int i = 0; i<copia.darTamano(); i++) {
+		for (int i = 0; i < copia.darTamano(); i++) {
 			cop = copia.dequeue();
 			test = Integer.parseInt(cop[0]);
-			if(test < menor) {
+			if (test < menor) {
 				menor = test;
 				zona = cop;
 			}
-			if(test == 1) {
+			if (test == 1) {
 				return zona;
 			}
 		}
 		copia = datosW;
-		for(int i = 0; i<copia.darTamano(); i++) {
+		for (int i = 0; i < copia.darTamano(); i++) {
 			cop = copia.dequeue();
 			test = Integer.parseInt(cop[0]);
-			if(test < menor) {
+			if (test < menor) {
 				menor = test;
 				zona = cop;
 			}
-			if(test == 1) {
+			if (test == 1) {
 				return zona;
 			}
 		}
 		copia = datosM;
-		for(int i = 0; i<copia.darTamano(); i++) {
+		for (int i = 0; i < copia.darTamano(); i++) {
 			cop = copia.dequeue();
 			test = Integer.parseInt(cop[0]);
-			if(test < menor) {
+			if (test < menor) {
 				menor = test;
 				zona = cop;
 			}
-			if(test == 1) {
+			if (test == 1) {
 				return zona;
 			}
 		}
 		return zona;
 	}
-	
+
 	public String[] mayorIdentificador() {
 		String[] zona = null;
 		String[] cop = null;
 		Queue<String[]> copia = datosH;
 		int mayor = 0;
 		int test = 0;
-		for(int i = 0; i<copia.darTamano(); i++) {
+		for (int i = 0; i < copia.darTamano(); i++) {
 			cop = copia.dequeue();
 			test = Integer.parseInt(cop[0]);
-			if(test > mayor) {
+			if (test > mayor) {
 				mayor = test;
 				zona = cop;
 			}
-			if(test == 1160) {
+			if (test == 1160) {
 				return zona;
 			}
 		}
 		copia = datosW;
-		for(int i = 0; i<copia.darTamano(); i++) {
+		for (int i = 0; i < copia.darTamano(); i++) {
 			cop = copia.dequeue();
 			test = Integer.parseInt(cop[0]);
-			if(test > mayor) {
+			if (test > mayor) {
 				mayor = test;
 				zona = cop;
 			}
-			if(test == 1160) {
+			if (test == 1160) {
 				return zona;
 			}
 		}
 		copia = datosM;
-		for(int i = 0; i<copia.darTamano(); i++) {
+		for (int i = 0; i < copia.darTamano(); i++) {
 			cop = copia.dequeue();
 			test = Integer.parseInt(cop[0]);
-			if(test > mayor) {
+			if (test > mayor) {
 				mayor = test;
 				zona = cop;
 			}
-			if(test == 1160) {
+			if (test == 1160) {
 				return zona;
 			}
 		}
 		return zona;
 	}
-	
-	
+
+	// PARTE A
+	/**
+	 * 1A Consultar el tiempo promedio de viaje y su desviación estándar de los
+	 * viajes entre una zona de origen y una zona destino para un mes dado.
+	 * 
+	 * @return String de tiempo promedio y desviación estándar
+	 */
 	public String tiempoPromedioDesviacionMes(String zonaOrigen, String zonaDestino, String mes) {
 		String rta = "";
 		int tam = datosM.darTamano();
-		Queue <String[]> copia = datosM;
-		Queue <String[]> zonas = new Queue<String[]>();
-		for (int i = 0; i <= tam-1; i++) {
+		Queue<String[]> copia = datosM;
+		Queue<String[]> zonas = new Queue<String[]>();
+		for (int i = 0; i <= tam - 1; i++) {
 			String[] actual = copia.dequeue();
 			if (actual[0].equals(zonaOrigen) && actual[1].equals(zonaDestino) && actual[3].equals(mes)) {
 				zonas.enqueue(actual);
@@ -179,20 +194,63 @@ public class MVCModelo {
 		}
 		if (zonas.isEmpty()) {
 			rta = null;
-		}
-		else {
+		} else {
 			double tPromedio = tiempoPromedio(zonas);
 			double desvEstPromedio = desviacionEstandarPromedio(zonas);
 			rta = tPromedio + ", " + desvEstPromedio;
 		}
 		return rta;
 	}
-	
+
+	/**
+	 * 2A Consultar la información de los N viajes con mayor tiempo promedio para un
+	 * mes dado.
+	 * 
+	 * @return Queue de los viajes ordenados
+	 */
+	public Queue<String[]> tiempoPromViajesMes(String N, String mes) {
+		int tam = datosM.darTamano();
+		Queue<String[]> viajesMes = new Queue<String[]>();
+		for (int i = 0; i < tam; i++) {
+			String[] actual = datosM.dequeue();
+			if (Integer.parseInt(actual[2]) == Integer.parseInt(mes)) {
+				viajesMes.enqueue(actual);
+			}
+		}
+		Queue<String[]> ordenados = ordenarViajesQuickSort(viajesMes);
+		Queue<String[]> nViajes = new Queue<String[]>();
+		for (int i = 0; i < Integer.parseInt(N); i++) {
+			nViajes.enqueue(ordenados.dequeue());
+		}
+		return nViajes;
+	}
+	/**
+	 * 3A Comparar los tiempos promedios de los viajes para una zona dada contra
+	 * cada zona X en un rango de zonas dado [Zona menor, Zona Mayor] en ambos
+	 * sentidos (zona dada –zona X vs. zona X –zona dada) para un mes dado. Mostrar
+	 * los resultados de comparación con cada zona X en una línea de la siguiente
+	 * forma: <tiempo promedio> de <Zona dada> a <Zona X> vs <tiempo promedio> de
+	 * <Zona X> a <Zona dada> Si no hay viajes en algún sentido o en ambos entre la
+	 * zona dada y una zona X debe reemplazarse <tiempo promedio> por "No hay
+	 * viajes" (en el sentido respectivo). Los resultados deben estar ordenados
+	 * ascendentemente por el identificador de la zona X en el rango dado.
+	 * 
+	 * @return
+	 */
+
+	// PARTE B
+	/**
+	 * 1B Consultar el tiempo promedio de viaje y su desviación estándar de los
+	 * viajes entre una zona de origen y una zona destino para un día dado de la
+	 * semana.
+	 * 
+	 * @return String de tiempo promedio y desviación estándar
+	 */
 	public String tiempoPromedioDesviacionDia(String zonaOrigen, String zonaDestino, String dia) {
 		String rta = "";
-		Queue <String[]> copia = datosW;
-		Queue <String[]> zonas = new Queue<String[]>();
-		for (int i = 0; i <= datosW.darTamano()-1; i++) {
+		Queue<String[]> copia = datosW;
+		Queue<String[]> zonas = new Queue<String[]>();
+		for (int i = 0; i <= datosW.darTamano() - 1; i++) {
 			String[] actual = copia.dequeue();
 			if (actual[0].equals(zonaOrigen) && actual[1].equals(zonaDestino) && actual[3].equals(dia)) {
 				zonas.enqueue(actual);
@@ -200,29 +258,145 @@ public class MVCModelo {
 		}
 		if (zonas.isEmpty()) {
 			rta = null;
-		}
-		else {
+		} else {
 			double tPromedio = tiempoPromedio(zonas);
 			double desvEstPromedio = desviacionEstandarPromedio(zonas);
 			rta = tPromedio + ", " + desvEstPromedio;
 		}
 		return rta;
 	}
-	
+
+	/**
+	 * 2B Consultar la información de los N viajes con mayor tiempo promedio para un
+	 * día dado.
+	 * 
+	 * @return Queue de los viajes ordenados
+	 */
+	public Queue<String[]> tiempoPromViajesDia(String N, String dia) {
+		int tam = datosW.darTamano();
+		Queue<String[]> viajesDia = new Queue<String[]>();
+		for (int i = 0; i < tam; i++) {
+			String[] actual = datosW.dequeue();
+			if (Integer.parseInt(actual[2]) == Integer.parseInt(dia)) {
+				viajesDia.enqueue(actual);
+			}
+		}
+		Queue<String[]> ordenados = ordenarViajesQuickSort(viajesDia);
+		Queue<String[]> nViajes = new Queue<String[]>();
+		for (int i = 0; i < Integer.parseInt(N); i++) {
+			nViajes.enqueue(ordenados.dequeue());
+		}
+		return nViajes;
+	}
+	/**
+	 * 3A Comparar los tiempos promedios de los viajes para una zona dada contra
+	 * cada zona X en un rango de zonas dado [Zona menor, Zona Mayor] en ambos
+	 * sentidos (zona dada –zona X vs. zona X –zona dada) para un día dado. Mostrar
+	 * los resultados de comparación con cada zona X en una línea de la siguiente
+	 * forma: <tiempo promedio> de <Zona dada> a <Zona X> vs <tiempo promedio> de
+	 * <Zona X> a <Zona dada> Si no hay viajes en algún sentido o en ambos entre la
+	 * zona dada y una zona X debe reemplazarse <tiempo promedio> por "No hay
+	 * viajes" (en el sentido respectivo). Los resultados deben estar ordenados
+	 * ascendentemente por el identificador de la zona X en el rango dado.
+	 * 
+	 * @return
+	 */
+
+	// PARTE C
+	/**
+	 * 1C Consultar los viajes entre una zona de origen y una zona destino en una
+	 * franja horaria (hora inicial –hora final) dada. La franja horaria se define
+	 * con horas enteras. Mostrar los viajes indicando el tiempo promedio de viaje y
+	 * su desviación estándar para cada hora entera iniciando en la hora inicial y
+	 * terminando en la hora final.
+	 * 
+	 * @return
+	 */
+
+	/**
+	 * 2C Consultar la información de los N viajes con mayor tiempo promedio para un
+	 * día dado.
+	 * 
+	 * @return Queue de los viajes ordenados
+	 */
+	public Queue<String[]> tiempoPromViajesHora(String N, String hora) {
+		int tam = datosH.darTamano();
+		Queue<String[]> viajesHora = new Queue<String[]>();
+		for (int i = 0; i < tam; i++) {
+			String[] actual = datosH.dequeue();
+			if (Integer.parseInt(actual[2]) == Integer.parseInt(hora)) {
+				viajesHora.enqueue(actual);
+			}
+		}
+		Queue<String[]> ordenados = ordenarViajesQuickSort(viajesHora);
+		Queue<String[]> nViajes = new Queue<String[]>();
+		for (int i = 0; i < Integer.parseInt(N); i++) {
+			nViajes.enqueue(ordenados.dequeue());
+		}
+		return nViajes;
+	}
+
+	/**
+	 * 32C Generar una gráfica ASCII que muestre el tiempo promedio de los viajes
+	 * entre una zona origen y una zona destino para cada hora del día. Cada * en la
+	 * gráfica corresponde a 1 minuto. Un tiempo promedio se aproxima a los minutos
+	 * más cercanos.
+	 * 
+	 * @return Arreglo con los viajes
+	 */
+	public String[] graficaASCII(String zonaOrigen, String zonaDestino) {
+		int minutos;
+		double tiempo;
+		String[] viajesAscii = new String[24];
+		String aster = "";
+		Queue<String[]> copia = datosH;
+		Queue<String[]> zonas = new Queue<String[]>();
+		for (int i = 0; i <= copia.darTamano() - 1; i++) {
+			String[] actual = copia.dequeue();
+			if (actual[0].equals(zonaOrigen) && actual[1].equals(zonaDestino)) {
+				zonas.enqueue(actual);
+			}
+		}
+		Queue<String[]> ascii = zonas;
+		for (int i = 0; i <= ascii.darTamano() - 1 && ascii != null; i++) {
+			String[] actual = ascii.dequeue();
+			for (int hor = 0; hor <= 23; hor++) {
+				if (actual[2].equals(String.valueOf(hor))) {
+					tiempo = Double.parseDouble(actual[3]);
+					minutos = (int) tiempo / 60;
+					for (int j = 0; j < minutos; j++) {
+						String a = "*";
+						aster = aster + a;
+					}
+					viajesAscii[hor] = "";
+					viajesAscii[hor] = viajesAscii[hor] + aster;
+				}
+			}
+		}
+		aster = "hora sin viajes";
+		for (int i = 0; i < viajesAscii.length; i++) {
+			if (viajesAscii[i] == null) {
+				viajesAscii[i] = aster;
+			}
+		}
+		return viajesAscii;
+	}
+
+	// MÉTODOS ÚTILES
 	public Queue<String[]> ordenarViajesQuickSort(Queue<String[]> datos) {
 		int izq = 0;
 		int der = datos.darTamano() - 1;
 		int pivote = quicksort(datos, izq, der);
-		if(izq < pivote-1 && pivote+1 > der) {
-			quicksort(datos, izq, pivote-1);
-			quicksort(datos, pivote+1, der);
-		}	
+		if (izq < pivote - 1 && pivote + 1 > der) {
+			quicksort(datos, izq, pivote - 1);
+			quicksort(datos, pivote + 1, der);
+		}
 		return datos;
 	}
-	
+
 	public int quicksort(Queue<String[]> datos, int izq, int der) {
 		String[] pivote = datos.darElemento(izq);
-		int i = izq-1;
+		int i = izq - 1;
 		String[] aux = new String[datos.darElemento(izq).length];
 		for (int j = izq; j < der; j++) {
 			if (Integer.parseInt(datos.darElemento(j)[3]) > Integer.parseInt(pivote[3])) {
@@ -236,17 +410,17 @@ public class MVCModelo {
 				}
 			}
 		}
-		aux = datos.darElemento(i+1);
+		aux = datos.darElemento(i + 1);
 		for (int k = 0; k < datos.darElemento(i).length; k++) {
-			datos.darElemento(i+1)[k] = datos.darElemento(der)[k];
+			datos.darElemento(i + 1)[k] = datos.darElemento(der)[k];
 		}
 		for (int k = 0; k < datos.darElemento(i).length; k++) {
 			datos.darElemento(der)[k] = aux[k];
 		}
-		return i+1;
+		return i + 1;
 	}
-	
-	public double tiempoPromedio(Queue<String[]> datos){
+
+	public double tiempoPromedio(Queue<String[]> datos) {
 		int tam = datos.darTamano();
 		double tiempo = 0;
 		double tPromedio = 0;
@@ -254,10 +428,10 @@ public class MVCModelo {
 			String[] actual = datos.dequeue();
 			tiempo = tiempo * Double.valueOf(actual[3]);
 		}
-		tPromedio = (tiempo/tam);
+		tPromedio = (tiempo / tam);
 		return tPromedio;
 	}
-	
+
 	public double desviacionEstandarPromedio(Queue<String[]> datos) {
 		int tam = datos.darTamano();
 		double desvEst = 0;
@@ -266,57 +440,7 @@ public class MVCModelo {
 			String[] actual = datos.dequeue();
 			desvEst = desvEst * Double.valueOf(actual[4]);
 		}
-		desvEstPromedio = (desvEst/tam);
+		desvEstPromedio = (desvEst / tam);
 		return desvEstPromedio;
 	}
-	
-	public Queue<String[]> tiempoPromViajesMes(String N, String mes) {
-		int tam = datosM.darTamano();
-		Queue<String[]> viajesMes = new Queue<String[]>();
-		for (int i = 0; i < tam; i++) {
-			String [] actual = datosM.dequeue();
-			if (Integer.parseInt(actual[2]) == Integer.parseInt(mes)) {
-				viajesMes.enqueue(actual);
-			}
-		}
-		Queue<String[]> ordenados = ordenarViajesQuickSort(viajesMes);
-		Queue<String[]> nViajes = new Queue<String[]>();
-		for (int i = 0; i < Integer.parseInt(N); i++) {
-			nViajes.enqueue(ordenados.dequeue());
-		}
-		return nViajes;
-	}
-	public Queue<String[]> tiempoPromViajesDia(String N, String dia) {
-		int tam = datosW.darTamano();
-		Queue<String[]> viajesDia = new Queue<String[]>();
-		for (int i = 0; i < tam; i++) {
-			String [] actual = datosW.dequeue();
-			if (Integer.parseInt(actual[2]) == Integer.parseInt(dia)) {
-				viajesDia.enqueue(actual);
-			}
-		}
-		Queue<String[]> ordenados = ordenarViajesQuickSort(viajesDia);
-		Queue<String[]> nViajes = new Queue<String[]>();
-		for (int i = 0; i < Integer.parseInt(N); i++) {
-			nViajes.enqueue(ordenados.dequeue());
-		}
-		return nViajes;
-	}
-	public Queue<String[]> tiempoPromViajesHora(String N, String hora) {
-		int tam = datosH.darTamano();
-		Queue<String[]> viajesHora = new Queue<String[]>();
-		for (int i = 0; i < tam; i++) {
-			String [] actual = datosH.dequeue();
-			if (Integer.parseInt(actual[2]) == Integer.parseInt(hora)) {
-				viajesHora.enqueue(actual);
-			}
-		}
-		Queue<String[]> ordenados = ordenarViajesQuickSort(viajesHora);
-		Queue<String[]> nViajes = new Queue<String[]>();
-		for (int i = 0; i < Integer.parseInt(N); i++) {
-			nViajes.enqueue(ordenados.dequeue());
-		}
-		return nViajes;
-	}
-	
 }
